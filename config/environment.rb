3288ce368142a3e5f7ae8b658864a4fc69a78689
github username <rss3509@gmail.com>
2020-08-12 19:30:25 -0700

    files

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+require 'bundler'
+Bundler.require
+
+require_relative '../app/profile.rb'
+require_relative '../lib/command_line_interface.rb'
+require_relative '../app/site.rb'
+require_relative '../app/user.rb'
