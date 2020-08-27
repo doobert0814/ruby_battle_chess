decc7cd3b621c20ba5921f33b19f6641db91cf30
github username <rss3509@gmail.com>
2020-08-26 17:48:39 -0700

    changed the board

--- app/board.rb
+++ app/board.rb
@@ -1,93 +1,129 @@
-class board
-    attr_accessor :squares
+require 'pry'
+require 'colorize'
 
-    def initialize(player_1, player_2)
-        @player_1 = player_1
-        @player_2 = player_2
-        @squares = Array.new(64, " ")
-        start_pos
+class Board
+    attr_reader :squares, :rows, :columns
 
-
-        @positions = [  [a,1], [a,2],[a,3],[a,3],[a,4],[a,5],[a,6],[a,7], [a,8], 
-                        [b,1], [b,2],[b,3],[b,3],[b,4],[b,5],[b,6],[b,7], [b,8],
-                        [c,1], [c,2],[c,3],[c,3],[c,4],[c,5],[c,6],[c,7], [c,8], 
-                        [d,1], [d,2],[d,3],[d,3],[d,4],[d,5],[d,6],[d,7], [d,8], 
-                        [e,1], [e,2],[e,3],[e,3],[e,4],[e,5],[e,6],[e,7], [e,8], 
-                        [f,1], [f,2],[f,3],[f,3],[f,4],[f,5],[f,6],[f,7], [f,8], 
-                        [g,1], [g,2],[g,3],[g,3],[g,4],[g,5],[g,6],[g,7], [g,8], 
-                        [h,1], [h,2],[h,3],[h,3],[h,4],[h,5],[h,6],[h,7], [h,8]]
-        
+    def initialize
+        @rows = SIZE
+        @columns = SIZE
+        @bg_colors = [:light_white, :cyan]
+        create_squares(rows, columns)
     end
 
-    def start_pos
-        #####################Player_1 Pieces############################
-
-        @squares[48] = @player_1.pawn.piece
-        @player_1.pawn1.position = [g,1]
-        @squares[49] = @player_1.pawn.piece
-        @player_1.pawn2.position = [g,2]
-        @squares[50] = @player_1.pawn.piece
-        @player_1.pawn3.position = [g,3]
-        @squares[51] = @player_1.pawn.piece
-        @player_1.pawn4.position = [g,4]
-        @squares[52] = @player_1.pawn.piece
-        @player_1.pawn5.position = [g,5]
-        @squares[53] = @player_1.pawn.piece
-        @player_1.pawn6.position = [g,6]
-        @squares[54] = @player_1.pawn.piece
-        @player_1.pawn7.position = [g,7]
-        @squares[55] = @player_1.pawn.piece
-
-        #####################Player_2 Pieces############################
-
-        @player_1.pawn8.position = [g,8]
-        @squares[8] = @player_1.pawn.piece
-        @player_2.pawn1.position = [g,1]
-        @squares[9] = @player_2.pawn.piece
-        @player_2.pawn2.position = [g,2]
-        @squares[10] = @player_2.pawn.piece
-        @player_2.pawn3.position = [g,3]
-        @squares[11] = @player_2.pawn.piece
-        @player_2.pawn4.position = [g,4]
-        @squares[12] = @player_2.pawn.piece
-        @player_2.pawn5.position = [g,5]
-        @squares[13] = @player_2.pawn.piece
-        @player_2.pawn6.position = [g,6]
-        @squares[14] = @player_2.pawn.piece
-        @player_2.pawn7.position = [g,7]
-        @squares[15] = @player_2.pawn.piece
-        @player_2.pawn8.position = [g,8]
+######################Create Squares###############################
+    def create_squares(rows, columns)
+        @squares = Array.new(rows) {Array.new(columns)}
     end
 
-    def show(piece = nil, start_position = [nil,nil], finish_position = [nil,nil])
-        piece.position = finish_position if !piece.nil?
-        i = 0
-        @positions.each do |position|
-            i += 1
-            if position == start_position
-            @squares[i-1] = " "
-            end
-            if position == finish_position
-            @squares[i-1] = piece.piece
-            end
+######################Draw Board###############################
+    def draw_board
+        draw_squares(@bg_colors[0])
+        show_bottom_letters
+    end
+
+######################Create bottom letters###############################
+    def show_bottom_letters
+        puts "".center(6)+(97.chr..(97+7).chr).to_a.join("".center(7))
+    end
+
+######################Draw Squares###############################
+    def draw_squares(bg_color)
+        rows.times do |row|
+            puts "".center(3) + draw_columns(bg_color)
+            puts "#{rows-row}".center(3) + draw_columns(row,bg_color)
+            puts "".center(3) + draw_columns(bg_color)
+            bg_color = change_color(bg_color)
         end
-                puts "\n", " --+---+---+---+---+---+---+---+---+"
-                puts " 8 | #{@squares[57]} | #{@squares[58]} | #{@squares[59]} | #{@squares[60]} | #{@squares[61]} | #{@squares[62]} | #{@squares[63]} | #{@squares[64]} | => #{@player_1.name}"
-                puts " --+---+---+---+---+---+---+---+---+"
-                puts " 7 | #{@squares[49]} | #{@squares[50]} | #{@squares[51]} | #{@squares[52]} | #{@squares[53]} | #{@squares[54]} | #{@squares[55]} | #{@squares[56]} |"
-                puts " --+---+---+---+---+---+---+---+---+"
-                puts " 6 | #{@squares[41]} | #{@squares[42]} | #{@squares[43]} | #{@squares[44]} | #{@squares[45]} | #{@squares[46]} | #{@squares[47]} | #{@squares[48]} |"
-                puts " --+---+---+---+---+---+---+---+---+"
-                puts " 5 | #{@squares[33]} | #{@squares[34]} | #{@squares[35]} | #{@squares[36]} | #{@squares[37]} | #{@squares[38]} | #{@squares[39]} | #{@squares[40]} |"
-                puts " --+---+---+---+---+---+---+---+---+"
-                puts " 4 | #{@squares[25]} | #{@squares[26]} | #{@squares[27]} | #{@squares[28]} | #{@squares[29]} | #{@squares[30]} | #{@squares[31]} | #{@squares[32]} |"
-                puts " --+---+---+---+---+---+---+---+---+"
-                puts " 3 | #{@squares[17]} | #{@squares[18]} | #{@squares[19]} | #{@squares[20]} | #{@squares[21]} | #{@squares[22]} | #{@squares[23]} | #{@squares[24]} |"
-                puts " --+---+---+---+---+---+---+---+---+"
-                puts " 2 | #{@squares[9]} | #{@squares[10]} | #{@squares[11]} | #{@squares[12]} | #{@squares[13]} | #{@squares[14]} | #{@squares[15]} | #{@squares[16]} |"
-                puts " --+---+---+---+---+---+---+---+---+"
-                puts " 1 | #{@squares[1]} | #{@squares[2]} | #{@squares[3]} | #{@squares[4]} | #{@squares[5]} | #{@squares[6]} | #{@squares[7]} | #{@squares[8]} | => #{@player_2.name}"
-                puts " --+---+---+---+---+---+---+---+---+"
-                puts "   | a | b | c | d | e | f | g | h |", "\n"
+    end
+
+######################Draw Columns###############################
+    def draw_columns(row=nil,bg_color)
+        line = ""
+        columns.times do |column|
+            line += get_value_in(row,column).center(8).colorize(:color => :black, :background => bg_color)
+            bg_color = change_color(bg_color)
+        end
+        line
     end
 end
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ###############################################################################################
+        # @player_1 = player_1
+        # @player_2 = player_2
+        # @squares = Array.new(64, " ")
+        # start_pos
+
+
+#         @positions = [  [a,1], [a,2],[a,3],[a,3],[a,4],[a,5],[a,6],[a,7], [a,8], 
+#                         [b,1], [b,2],[b,3],[b,3],[b,4],[b,5],[b,6],[b,7], [b,8],
+#                         [c,1], [c,2],[c,3],[c,3],[c,4],[c,5],[c,6],[c,7], [c,8], 
+#                         [d,1], [d,2],[d,3],[d,3],[d,4],[d,5],[d,6],[d,7], [d,8], 
+#                         [e,1], [e,2],[e,3],[e,3],[e,4],[e,5],[e,6],[e,7], [e,8], 
+#                         [f,1], [f,2],[f,3],[f,3],[f,4],[f,5],[f,6],[f,7], [f,8], 
+#                         [g,1], [g,2],[g,3],[g,3],[g,4],[g,5],[g,6],[g,7], [g,8], 
+#                         [h,1], [h,2],[h,3],[h,3],[h,4],[h,5],[h,6],[h,7], [h,8]]
+        
+#     end
+
+#     def start_pos
+#         #####################Player_1 Pieces white ############################
+
+#         @squares[48] = @player_1.pawn.piece
+#         @player_1.pawn1.position = [g,1]
+#         @squares[49] = @player_1.pawn.piece
+#         @player_1.pawn2.position = [g,2]
+#         @squares[50] = @player_1.pawn.piece
+#         @player_1.pawn3.position = [g,3]
+#         @squares[51] = @player_1.pawn.piece
+#         @player_1.pawn4.position = [g,4]
+#         @squares[52] = @player_1.pawn.piece
+#         @player_1.pawn5.position = [g,5]
+#         @squares[53] = @player_1.pawn.piece
+#         @player_1.pawn6.position = [g,6]
+#         @squares[54] = @player_1.pawn.piece
+#         @player_1.pawn7.position = [g,7]
+#         @squares[55] = @player_1.pawn.piece
+
+#         #####################Player_2 Pieces black ############################
+
+#         @player_2.pawn8.position = [g,8]
+#         @squares[8] = @player_2.pawn.piece
+#         @player_2.pawn1.position = [g,1]
+#         @squares[9] = @player_2.pawn.piece
+#         @player_2.pawn2.position = [g,2]
+#         @squares[10] = @player_2.pawn.piece
+#         @player_2.pawn3.position = [g,3]
+#         @squares[11] = @player_2.pawn.piece
+#         @player_2.pawn4.position = [g,4]
+#         @squares[12] = @player_2.pawn.piece
+#         @player_2.pawn5.position = [g,5]
+#         @squares[13] = @player_2.pawn.piece
+#         @player_2.pawn6.position = [g,6]
+#         @squares[14] = @player_2.pawn.piece
+#         @player_2.pawn7.position = [g,7]
+#         @squares[15] = @player_2.pawn.piece
+#         @player_2.pawn8.position = [g,8]
+#     end
+
+#     ########################Draw_Squares#######################
+
+#     def draw_squares
+  
+
+binding.pry 
+0
--- app/pieces/pawn.rb
+++ app/pieces/pawn.rb
@@ -1,6 +1,31 @@
-class Pawn < Piece
-    attr_accessor :initial_square, :color
+# A pawn can move forward one space and back one space in standard movement.
+# A pawn can only attack to a forward diagonal
+
+class Pawn < piece
+    attr_accessor :color, :icon, :move_count, :status
+    attr_reader :name
+  
     def initialize(color)
-        
+        @name = "Pawn"
+        @color = color
+        @moves = 0
+        @status = "active"
+
+        case
+        when @color == "black"
+            @icon = "♟".black
+        when @color == "white"
+            @icon = "♙".black
+        end
     end
+
+    def moves
+      
+    end
+
+    def straigh_move
+    end
+
+    def diag_moves
+  
 end
